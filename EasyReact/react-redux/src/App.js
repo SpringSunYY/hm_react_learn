1425f21b18eab77aef83872f59b2c459074f2fba
SpringSunYY <3217778348@qq.com>
2024-12-08 17:40:36 +0800

    美团案例

--- EasyReact/react-redux/src/App.js
+++ EasyReact/react-redux/src/App.js
@@ -1,14 +1,30 @@
 import {useDispatch, useSelector} from "react-redux";
-import {addToNum} from "./store/modules/counterStore";
+import {addToNum, decrement, inscrement} from "./store/modules/counterStore";
+import {useEffect} from "react";
+import {fetchChannlList} from "./store/modules/channelStore";
 
 function App() {
     const {count} = useSelector(state => state.counter)
     const dispatch = useDispatch()
+
+    const {channelList} = useSelector(state => state.channel)
+    //使用useEffect触发异步执行
+    useEffect(() => {
+        dispatch(fetchChannlList())
+    }, [dispatch]);
     return (
         <div className="App">
+            <button onClick={() => dispatch(decrement())}>-</button>
+            {count}
+            <button onClick={() => dispatch(inscrement())}>+</button>
+            <br/>
             <button onClick={() => dispatch(addToNum(10))}>add To 10</button>
             {count}
             <button onClick={() => dispatch(addToNum(20))}>add To 20</button>
+            <br/>
+            <ul>
+                {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+            </ul>
         </div>
     );
 }
