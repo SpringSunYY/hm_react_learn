56dc2cc1994a7bb3357027da5076edf7c5d68301
SpringSunYY <3217778348@qq.com>
2024-12-15 14:49:05 +0800

    useMemo

--- react-pro/src/App.js
+++ react-pro/src/App.js
@@ -1,10 +1,12 @@
 import UseReducer from "./useReducer";
+import UseMemo from "./useMemo";
 
 function App() {
 
     return (
         <>
             <UseReducer/>
+            <UseMemo/>
         </>
     )
 }
